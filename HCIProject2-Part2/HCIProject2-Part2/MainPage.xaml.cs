a22576ec4e0446181d9d4cdb5cd9c5dd5eac6136
Kamalpreet Mundi <kamalmundi31@gmail.com>
2021-10-24 17:13:33 -0600

    Add project files.

---
+++ HCIProject2-Part2/HCIProject2-Part2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HCIProject2_Part2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
