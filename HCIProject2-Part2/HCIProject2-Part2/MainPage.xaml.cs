09c97043927cd98ebf258d848299a7ceb5eeb67b
edwardm8 <edwardm825@gmail.com>
2021-10-24 18:15:41 -0600

    Just cirlce moving testing

--- HCIProject2-Part2/HCIProject2-Part2/MainPage.xaml.cs
+++ HCIProject2-Part2/HCIProject2-Part2/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace HCIProject2_Part2
@@ -13,6 +14,20 @@ namespace HCIProject2_Part2
         public MainPage()
         {
             InitializeComponent();
+            if (Accelerometer.IsMonitoring)
+                return;
+
+            // Register and Staart Accelermeter
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
+        }
+
+        async void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
+            await ballEllipse.TranslateTo(e.Reading.Acceleration.X * 100, e.Reading.Acceleration.Y * 100, 1000);
         }
     }
+    
 }
