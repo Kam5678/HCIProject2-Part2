74b0472c3306d660b7288004de4dfbd8f0d8b70e
Kamalpreet Mundi <kamalmundi31@gmail.com>
2021-10-25 18:53:42 -0600

    Sample star moves left and right

--- HCIProject2-Part2/HCIProject2-Part2/MainPage.xaml.cs
+++ HCIProject2-Part2/HCIProject2-Part2/MainPage.xaml.cs
@@ -7,11 +7,114 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Essentials;
+using SkiaSharp.Views.Forms;
+using SkiaSharp;
+using System.Threading;
+using System.Diagnostics;
 
 namespace HCIProject2_Part2
 {
     public partial class MainPage : ContentPage
     {
+        // https://www.c-sharpcorner.com/article/getting-started-with-skiasharp-with-xamarin-forms/
+        // https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/graphics/skiasharp/transforms/translate
+
+        public static readonly SKPath HendecagramPath;
+        const double cycleTime = 2000;      // in milliseconds
+
+        SKCanvasView canvasView;
+        System.Diagnostics.Stopwatch stopwatch = new Stopwatch();
+        bool pageIsActive;
+        float angle = -90;
+
+        public MainPage()
+        {
+            Title = "Hedecagram Animation";
+
+            canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+
+            if (Accelerometer.IsMonitoring)
+                return;
+
+            // Register and Staart Accelermeter
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
+        }
+
+         protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            pageIsActive = true;
+            stopwatch.Start();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(33), () =>
+            {
+                //var data = e.Reading;
+                //double t = stopwatch.Elapsed.TotalMilliseconds % cycleTime / cycleTime;
+                //double t = data.Acceleration.X;
+                //angle = (float)(-90+(180 * t));
+                //Console.WriteLine(angle);
+                Console.WriteLine(angle);
+                canvasView.InvalidateSurface();
+
+                if (!pageIsActive)
+                {
+                    stopwatch.Stop();
+                }
+
+                return pageIsActive;
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            pageIsActive = false;
+        }
+        static MainPage()
+        {
+            // Create 11-pointed star
+            HendecagramPath = new SKPath();
+            for (int i = 0; i < 11; i++)
+            {
+                double angle = 5 * i * 2 * Math.PI / 11;
+                SKPoint pt = new SKPoint(100 * (float)Math.Sin(angle),
+                                        -100 * (float)Math.Cos(angle));
+                if (i == 0)
+                {
+                    HendecagramPath.MoveTo(pt);
+                }
+                else
+                {
+                    HendecagramPath.LineTo(pt);
+                }
+            }
+            HendecagramPath.Close();
+        }
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+            canvas.Translate(info.Width / 2, info.Height / 2);
+            float radius = (float)Math.Min(info.Width, info.Height) / 2 - 100;
+
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.Style = SKPaintStyle.Fill;
+                paint.Color = SKColor.FromHsl(angle, 100, 50);
+
+                float x = radius * (float)Math.Sin(Math.PI * angle / 180);
+                float y = -radius * (float)Math.Cos(Math.PI * angle / 180);
+                canvas.Translate(x, y);
+                canvas.DrawPath(MainPage.HendecagramPath, paint);
+            }
+        }
+        /**
         public MainPage()
         {
             InitializeComponent();
@@ -21,14 +124,83 @@ namespace HCIProject2_Part2
             // Register and Staart Accelermeter
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
             Accelerometer.Start(SensorSpeed.UI);
+            
+
+            //get the canvas & info
+            
+
         }
+        **/
+
+
 
         async void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
             var data = e.Reading;
-            Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
-            await ballEllipse.TranslateTo(e.Reading.Acceleration.X * 100, e.Reading.Acceleration.Y * 100, 1000);
+            angle = (float)(0 - (90 * data.Acceleration.X));
+            //Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
+            //await ballEllipse.TranslateTo(e.Reading.Acceleration.X * -200, e.Reading.Acceleration.Y * 200, 200);
         }
+
+        /**
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            //Initialize the Canvas  
+            SKSurface vSurface = args.Surface;
+            SKCanvas vCanvas = vSurface.Canvas;
+            int surfaceWidth = args.Info.Width;
+            int surfaceHeight = args.Info.Height;
+            float radius = (Math.Min(surfaceHeight, surfaceWidth) * 0.5f) - 25;
+            //Clear the Canvas  
+            vCanvas.Clear();
+            //Creating the Paint object to color the Items  
+            SKPaint vBlackPaint = new SKPaint
+            {
+                Color = SKColors.Black,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+                TextSize = 60
+            };
+            SKPaint vWhitePaint = new SKPaint
+            {
+                Color = SKColors.White
+            };
+            var outerPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke, //stroke so that it traces the outline
+                Color = Color.DarkBlue.ToSKColor(), //make it the color red
+                StrokeWidth = 25
+            };
+            var innerPaint = new SKPaint()
+            {
+                Style = SKPaintStyle.Fill,
+                Color = Color.LightBlue.ToSKColor(),
+            };
+
+
+            //vCanvas.DrawCircle(surfaceWidth / 2, surfaceHeight / 2, radius, outerPaint);
+            //vCanvas.DrawCircle(surfaceWidth / 2, surfaceHeight / 2, radius, innerPaint);
+
+            
+            var angle = Math.PI * (startAngle + positionOfMarker) / 180.0;
+
+            //calculate the radius and the center point of the circle
+            var radius = (originalRect.Right - originalRect.Left) / 2;
+            var middlePoint = new SKPoint();
+            middlePoint.X = (originalRect.Left + radius);
+            middlePoint.Y = originalRect.Top + radius; //top of current circle plus radius
+
+            surface.Canvas.DrawCircle(middlePoint.X + (float)(radius * Math.Cos(angle)),
+            middlePoint.Y + (float)(radius * Math.Sin(angle)), 20, circlePaint);
+            
+
+
+
+
+        }
+
+        **/
+
     }
     
 }
